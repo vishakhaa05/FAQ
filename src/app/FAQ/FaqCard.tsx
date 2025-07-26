bbd57b72d882c76b585dc976bbae2688c1c541e2
vishakhaa05 <vishakhaagrawal0508@gmail.com>
2025-07-26 12:46:12 +0530

    Your commit message

---
+++ src/app/FAQ/FaqCard.tsx
@@ -0,0 +1,21 @@
+'use client'
+import { useState } from 'react'
+type FaqCardProps={
+    question: string 
+    answer : string
+}
+export default function FaqCard({ question, answer }: FaqCardProps){
+    const [isOpen, setIsOpen] = useState(false)
+    return(
+        <div className="bg-white shadow-md rounded-2xl p-4 mb-4 transition-all duration-300">
+            <button onClick={() => setIsOpen(!isOpen)}
+        className="w-full flex justify-between items-center text-left">
+            <h3 className="text-lg font-semibold">{question}</h3>
+        <span className="text-2xl">{isOpen ? '−' : '+'}</span>
+      </button>
+        {isOpen && (
+        <p className="mt-3 text-gray-600 transition-all duration-300">{answer}</p>
+      )}
+        </div>
+    )
+}
